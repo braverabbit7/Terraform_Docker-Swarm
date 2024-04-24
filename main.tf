f35b0d0dcc5518b59b04a89e2b6d73c406fd4e0d
Semyon Adaikin <brave.rabbit@gmail.com>
2024-04-24 13:52:11 +0300

    Done

---
+++ main.tf
@@ -0,0 +1,32 @@
+terraform {
+  required_providers {
+    yandex = {
+      source  = "yandex-cloud/yandex"
+      version = "0.104.0"
+    }
+  }
+}
+provider "yandex" {
+  cloud_id                 = var.cloud_id
+  folder_id                = var.folder_id
+  service_account_key_file = pathexpand("~/.ssh/authorized_key.json")
+  zone                     = "ru-central1-a"
+}
+
+resource "yandex_vpc_network" "network" {
+  name = "swarm-network"
+}
+
+resource "yandex_vpc_subnet" "subnet" {
+  name           = "subnet1"
+  zone           = "ru-central1-a"
+  network_id     = yandex_vpc_network.network.id
+  v4_cidr_blocks = ["192.168.10.0/24"]
+}
+
+module "swarm_cluster" {
+  source        = "./modules/instance"
+  vpc_subnet_id = yandex_vpc_subnet.subnet.id
+  managers      = 1
+  workers       = 2
+}
